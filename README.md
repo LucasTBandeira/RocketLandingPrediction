cc357396c1ef7ef491e9a15e24385891d06592b1
LucasBandeira <lucastbandeira@gmail.com>
2024-01-25 01:27:24 -0300

    README update

--- README.md
+++ README.md
@@ -1,2 +1,101 @@
 # Winning the Space Race with Data Science
 
+<img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0701EN-SkillsNetwork/api/Images/landing_1.gif" alt="Space Race" style="width:100%;">
+
+## Background
+
+Space exploration is experiencing unprecedented growth, with a growing interest in the commercial aspects of this field. SpaceX is at the forefront of making space travel more economically viable through a key strategy for cost reduction: the reusability of the first stage of its Falcon 9 rockets.
+
+Currently, SpaceX launches Falcon 9 rockets at a significantly lower cost of $62 million, in stark contrast to other providers whose prices typically exceed $165 million. In an effort to revolutionize the space industry and compete with SpaceX's pricing, it is crucial for competitors to gain insights into the factors affecting launch costs, which underscores the importance of predicting the successful landing of the Falcon 9 first stage.
+
+This project aims to predict whether the SpaceX Falcon 9 first stage will successfully land after launch. By making accurate predictions, we can estimate the cost of a launch, providing valuable information for competitors seeking to optimize their pricing strategies.
+
+## Author
+
+[@LucasBandeira](https://github.com/LucasTBandeira)
+
+## Table of Contents
+
+- [Application](#application)
+- [Tech Stack](#tech-stack)
+- [Usage](#usage)
+- [Data Collection](#data-collection)
+- [Data Analysis](#data-analysis)
+- [Results](#results)
+
+## Application
+
+A user-friendly **[dashboard](https://spacex-rocket-landing-analysis.onrender.com/)** has been developed to provide an interactive exploration of SpaceX launch records. The dashboard is implemented using Dash, a Python framework for building analytical web applications. Users can visualize and analyze data related to SpaceX launches, including success rates, payload correlations, and launch site details.
+
+![Dashboard Screenshot](images/dashboard_01.jpg)
+![Dashboard Screenshot](images/dashboard_02.jpg)
+
+## Tech Stack
+
+- **Data Analysis:** Python, Pandas, NumPy
+
+- **Exploratory Data Analysis (EDA):** Matplotlib, Seaborn
+
+- **Geospatial Visualization:** Folium
+
+- **Predictive Analysis:** Scikit-Learn
+
+- **Dashboard:** Plotly, Dash, HTML, CSS
+
+## Usage
+
+To replicate and explore the analysis conducted in this project, follow these steps:
+
+1. Clone the repository to your local machine:
+
+```bash
+git clone https://github.com/your-username/SpaceRaceDataScience.git
+```
+
+2. Navigate to the project directory:
+
+```bash
+cd SpaceRaceDataScience
+```
+
+3. Install the required dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+
+4. Explore the Jupyter notebooks in the `notebooks/` directory for detailed analysis and insights.
+
+5. Utilize the processed data in the `data/` directory for your own analyses.
+
+## Data Collection
+
+The data collected from the [SpaceX API](https://documenter.getpostman.com/view/1559645/RVtvsEFr) involves extracting information about past launches, including details about the booster version, payload mass, orbit, launch site, and outcomes. In addition to API data, this project also gathers information through web scraping from [Wikipedia](https://en.wikipedia.org/w/index.php?title=List_of_Falcon_9_and_Falcon_Heavy_launches&oldid=1027686922). The web scraping code extracts data related to Falcon 9 and Falcon Heavy launches. All the collected data is then processed and organized into a structured dataset for further analysis.
+
+## Data Analysis
+
+### Data Wrangling
+
+The collected data goes through a data wrangling process to ensure its quality and prepare it for analysis. This includes filtering the data to focus on Falcon 9 launches, handling missing values, and fixing inconsistencies in the payload mass column.
+
+### Exploratory Data Analysis (EDA)
+
+In the EDA phase, an in-depth exploration and visualization of the data are conducted to extract insights into relationships and patterns. Various visualization techniques, including catplots, barplots, lineplots, and more, are employed to analyze critical factors such as flight number, payload mass, launch site, orbit type, success rate trends, and other pertinent features.
+
+Feature engineering involves selecting and transforming relevant features for analysis. Additionally, one-hot encoding is applied to categorical variables for better representation in machine learning models.
+
+The launch site analysis phase involves visualizing and marking the launch sites on a map. The project utilizes Folium, a Python library for creating interactive maps, to mark launch sites, visualize success/failure of launches, and calculate distances between the launch site and nearby landmarks.
+
+## Results
+
+In addition to exploratory data analysis, this project includes predictive analysis using machine learning models. The goal was to predict the success or failure of SpaceX Falcon 9 first-stage landings based on various features. Steps included:
+
+- Data Loading and Standardization
+- Train-Test Split
+- Model Selection (Logistic Regression, SVM, Decision Tree, KNN)
+- Hyperparameter Tuning
+- Model Evaluation and Results Visualization
+
+![Alt text](images/accuracy_by_ml_model.png)
+
+Decision Tree and SVM models had the best results in predicting Falcon 9 first stage landing, with 88.88% accuracy score.
--- src/app.py
+++ src/app.py
@@ -1,5 +1,4 @@
 # Import required libraries
-import os
 import pandas as pd
 import dash
 from dash import html
@@ -8,9 +7,9 @@ from dash.dependencies import Input, Output
 import plotly.express as px
 
 # Read the spacex data into pandas dataframe
-spacex_df = pd.read_csv('./assets/spacex_IBM_dash.csv')
-max_payload = spacex_df['Payload Mass (kg)'].max()
-min_payload = spacex_df['Payload Mass (kg)'].min()
+spacex_df = pd.read_csv("src/assets/spacex_IBM_dash.csv")
+max_payload = spacex_df["Payload Mass (kg)"].max()
+min_payload = spacex_df["Payload Mass (kg)"].min()
 
 # Create a dash application
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
@@ -18,26 +17,29 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 # Set the title of the dashboard
-app.title = 'SpaceX Launch Records Dashboard'
+app.title = "SpaceX Launch Records Dashboard"
 
 # Create an app layout
 app.layout = html.Div(
-    style={"backgroundColor": "#1E1E1E", "color": "white"}, 
+    style={"backgroundColor": "#1E1E1E", "color": "white"},
     children=[
         html.Br(),
-        html.H1('SpaceX Launch Records Dashboard', style={'textAlign': 'center', 'color': 'White', 'font-size': 40}),
-        
+        html.H1(
+            "SpaceX Launch Records Dashboard",
+            style={"textAlign": "center", "color": "White", "font-size": 40},
+        ),
         # Add a dropdown list to enable Launch Site selection
-        dcc.Dropdown(id='site-dropdown',
+        dcc.Dropdown(
+            id="site-dropdown",
             options=[
-                {'label': 'All sites', 'value': 'All'},
-                {'label': 'CCAFS LC-40', 'value': 'CCAFS LC-40'},
-                {'label': 'CCAFS SLC-40', 'value': 'CCAFS SLC-40'},
-                {'label': 'VAFB SLC-4E', 'value': 'VAFB SLC-4E'},
-                {'label': 'KSC LC-39A', 'value': 'KSC LC-39A'}
+                {"label": "All sites", "value": "All"},
+                {"label": "CCAFS LC-40", "value": "CCAFS LC-40"},
+                {"label": "CCAFS SLC-40", "value": "CCAFS SLC-40"},
+                {"label": "VAFB SLC-4E", "value": "VAFB SLC-4E"},
+                {"label": "KSC LC-39A", "value": "KSC LC-39A"},
             ],
-            value='All',
-            placeholder='Select a Launch Site here',
+            value="All",
+            placeholder="Select a Launch Site here",
             searchable=True,
             style={
                 "width": "60%",  # Reduce the width
@@ -47,80 +49,101 @@ app.layout = html.Div(
                 "text-align-last": "center",
                 "color": "black",
             },
-            ),
+        ),
         html.Br(),
-
         # Add a pie chart to show the total successful launches count for all sites
-        html.Div(dcc.Graph(id='success-pie-chart')),
+        html.Div(dcc.Graph(id="success-pie-chart")),
         html.Br(),
-        
-        html.Label("Payload Range (Kg)", style={'font-size': '15px', 'color': 'white', 'padding-left': '30px'}),
-                                
+        html.Label(
+            "Payload Range (Kg)",
+            style={"font-size": "15px", "color": "white", "padding-left": "30px"},
+        ),
         # Add a slider to select payload range
-        dcc.RangeSlider(id='payload-slider', 
-            min=0, max=10000, step=1000, 
-            marks={0:'0', 2500:'2500', 5000:'5000', 7500:'7500', 10000:'10000'},
-            value=[min_payload, max_payload]
-            ),
-
+        dcc.RangeSlider(
+            id="payload-slider",
+            min=0,
+            max=10000,
+            step=1000,
+            marks={0: "0", 2500: "2500", 5000: "5000", 7500: "7500", 10000: "10000"},
+            value=[min_payload, max_payload],
+        ),
         # Add a scatter chart to show the correlation between payload and launch success
-        html.Div(dcc.Graph(id='success-payload-scatter-chart')),
-        html.H2("Launches in Each Site", style={'textAlign': 'center', 'color': 'White', 'font-size': 30}),
-        html.Iframe(id = "map", srcDoc=open('./assets/launch_sites_map.html', 'r').read(), width='100%', height='600'),
-        ])
+        html.Div(dcc.Graph(id="success-payload-scatter-chart")),
+        html.H2(
+            "Launches in Each Site",
+            style={"textAlign": "center", "color": "White", "font-size": 30},
+        ),
+        html.Iframe(
+            id="map",
+            srcDoc=open("src/assets/launch_sites_map.html", "r").read(),
+            width="100%",
+            height="600",
+        ),
+    ],
+)
 
 
 # Add a callback function for `site-dropdown` as input, `success-pie-chart` as output
-@app.callback(Output(component_id='success-pie-chart', component_property='figure'),
-              Input(component_id='site-dropdown', component_property='value'))
-
+@app.callback(
+    Output(component_id="success-pie-chart", component_property="figure"),
+    Input(component_id="site-dropdown", component_property="value"),
+)
 def get_pie_chart(entered_site):
     filtered_df = spacex_df
-    if entered_site == 'All':
-        df = filtered_df.groupby(['Launch Site'])['class'].sum().to_frame()
+    if entered_site == "All":
+        df = filtered_df.groupby(["Launch Site"])["class"].sum().to_frame()
         df = df.reset_index()
-        fig = px.pie(df, values='class', names='Launch Site', title='Total success launches by site')
-    else:
-        df = filtered_df[filtered_df['Launch Site'] == entered_site]['class'].value_counts().to_frame()
-        df['name'] = ['Success', 'Failure']
-        fig = px.pie(df, values='count', names='name', title=f'Total Success Launches for {entered_site}')
-    
-    fig.update_layout(
-        template="plotly_dark",
-        font=dict(
-            size=15
+        fig = px.pie(
+            df,
+            values="class",
+            names="Launch Site",
+            title="Total success launches by site",
         )
-    )
-    
+    else:
+        df = (
+            filtered_df[filtered_df["Launch Site"] == entered_site]["class"]
+            .value_counts()
+            .to_frame()
+        )
+        df["name"] = ["Success", "Failure"]
+        fig = px.pie(
+            df,
+            values="count",
+            names="name",
+            title=f"Total Success Launches for {entered_site}",
+        )
+
+    fig.update_layout(template="plotly_dark", font=dict(size=15))
+
     return fig
 
 
 # Add a callback function for `site-dropdown` and `payload-slider` as inputs, `success-payload-scatter-chart` as output
-@app.callback(Output(component_id='success-payload-scatter-chart', component_property='figure'),
-              Input(component_id='site-dropdown', component_property='value'),
-              Input(component_id='payload-slider', component_property='value'))
-
+@app.callback(
+    Output(component_id="success-payload-scatter-chart", component_property="figure"),
+    Input(component_id="site-dropdown", component_property="value"),
+    Input(component_id="payload-slider", component_property="value"),
+)
 def get_scatter_plot(entered_site, slider_interval):
     df = spacex_df
-    if entered_site == 'All':
+    if entered_site == "All":
         df2 = df[df["Payload Mass (kg)"] >= slider_interval[0]]
         df3 = df2[df["Payload Mass (kg)"] <= slider_interval[1]]
-        fig2 = px.scatter(df3, y='class', x='Payload Mass (kg)', color='Booster Version Category')
-    else:
-        df = df[df['Launch Site'] == entered_site]
-        df2 = df[df["Payload Mass (kg)"] >= slider_interval[0]]
-        df3 = df2[df["Payload Mass (kg)"] <= slider_interval[1]]
-        fig2 = px.scatter(df3, y='class', x='Payload Mass (kg)', color='Booster Version Category')
-        
-    fig2.update_layout(
-        template="plotly_dark",
-        font=dict(
-            size=15
+        fig2 = px.scatter(
+            df3, y="class", x="Payload Mass (kg)", color="Booster Version Category"
         )
-    )
+    else:
+        df = df[df["Launch Site"] == entered_site]
+        df2 = df[df["Payload Mass (kg)"] >= slider_interval[0]]
+        df3 = df2[df["Payload Mass (kg)"] <= slider_interval[1]]
+        fig2 = px.scatter(
+            df3, y="class", x="Payload Mass (kg)", color="Booster Version Category"
+        )
+
+    fig2.update_layout(template="plotly_dark", font=dict(size=15))
     return fig2
-        
+
 
 # Run the app
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server()
